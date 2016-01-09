2432a02cf5a418c4f066696350e788ed6fb62613
Varun <date.varun123@gmail.com>
2016-01-09 20:31:01 +0530

    cmsdk: Add App SideBar Tile [2/2]
    Signed-off-by: Varun <date.varun123@gmail.com>

--- src/java/org/cyanogenmod/internal/util/QSConstants.java
+++ src/java/org/cyanogenmod/internal/util/QSConstants.java
@@ -63,6 +63,7 @@ public class QSConstants {
     public static final String TILE_NAVBAR = "navbar";	
     public static final String TILE_PIE = "pie";	
     public static final String TILE_APPCIRCLEBAR = "appcirclebar";
+    public static final String TILE_APPSIDEBAR = "appsidebar";
     public static final String TILE_RESTARTUI = "restartui";
 
     public static final String DYNAMIC_TILE_NEXT_ALARM = "next_alarm";
@@ -117,6 +118,7 @@ public class QSConstants {
         STATIC_TILES_AVAILABLE.add(TILE_NAVBAR);
         STATIC_TILES_AVAILABLE.add(TILE_PIE);
         STATIC_TILES_AVAILABLE.add(TILE_APPCIRCLEBAR);
+        STATIC_TILES_AVAILABLE.add(TILE_APPSIDEBAR);
         STATIC_TILES_AVAILABLE.add(TILE_RESTARTUI);
 
         TILES_AVAILABLE.addAll(STATIC_TILES_AVAILABLE);
